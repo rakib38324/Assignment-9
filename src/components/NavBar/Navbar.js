cc63050db7df98b94fe6012a6ade24eb1c2f6e24
rakib38324 <rakib3832@gmail.com>
2022-10-10 23:25:22 +0600

    Commit the navbar with responsibe

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import './NavBar.css'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>This is navbar</h1>
-        </div>
+        <nav className='w-full h-20 bg-blue-500 text-white sm:flex justify-around items-center'>
+            <div className='text-3xl '>
+                <p>Let's Play Gaming Quiz</p>
+            </div>
+            <div className='text-2xl '>
+            <Link className='pr-5' to='/'>Home</Link>
+            <Link className='pr-5' to='/'>Topics</Link>
+            <Link className='pr-5' to='/statistics'>Statistics</Link>
+            <Link className='pr-5' to='/blog'>Blog</Link>
+            </div>
+        </nav>
     );
 };
 
