f238de8a4398ca90dcf24c85c1c723240129d1f5
rakib38324 <rakib3832@gmail.com>
2022-10-10 23:46:21 +0600

    Commit the header section

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import img from './header-image.png'
 
 const Header = () => {
     return (
-        <div>
-            <h1>this is header section</h1>
+        <div className='bg-blue-100 sm:flex justify-center items-center'>
+            <div>
+            <img className='w-2/4' src={img} alt="" />
+            </div>
+            <div className='text-3xl'>
+                <p className='underline decoration-4'>Some Instructions</p>
+                <p>Don't Search Google</p>
+                <p>Don't any other source</p>
+                <p>Don't close window</p>         
+             </div>
         </div>
     );
 };
--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './NavBar.css'
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
