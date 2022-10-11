e323fa899dbb9f294f2df51911a379349d7de6fe
rakib38324 <rakib3832@gmail.com>
2022-10-11 20:17:07 +0600

    Add the toast for show correct answer

--- src/components/Quizlayout/Quizlayout.js
+++ src/components/Quizlayout/Quizlayout.js
@@ -2,8 +2,35 @@ import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye } from "@fortawesome/free-solid-svg-icons";
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const Quizlayout = ({ topic }) => {
-    const { question, options } = topic;
+    const { question, options, correctAnswer
+    } = topic;
+
+    const ToastAnswer = () => {
+        toast(`${correctAnswer
+            }`, {
+            position: "top-center"
+        });
+    }
+
+    const ToastAnswerbtn = (props) => {
+
+        if (props === correctAnswer) {
+            toast(`Congratulation!!! Your Answer is Correct`, {
+                position: "top-center"
+            });
+        }
+        else {
+            toast(`Sorry Your Answer is Wrong`, {
+                position: "top-center"
+            });
+        }
+    }
+
+
 
     return (
         <div className='mt-5'>
@@ -11,16 +38,19 @@ const Quizlayout = ({ topic }) => {
 
                 <div className='grid grid-cols-12 gap-1 col-end-1'>
                     <p className='text-3xl p-4 col-span-11'>{question}</p>
-                    <FontAwesomeIcon className="pl-4" icon={faEye}></FontAwesomeIcon>
+                    <FontAwesomeIcon onClick={ToastAnswer} className="pl-4" icon={faEye}></FontAwesomeIcon>
                 </div>
                 <div className=' sm:grid grid-cols-2 gap-8 content-center '>
-                    <p className='bg-blue-200 text-xl p-2 border-2 rounded-lg hover:bg-green-500 border-green-300'>{options[0]}</p>
-                    <p className='bg-blue-200 text-xl p-2 border-2 rounded-lg hover:bg-green-500 border-green-300'>{options[1]}</p>
-                    <p className='bg-blue-200 text-xl p-2 border-2 rounded-lg hover:bg-green-500 border-green-300'>{options[2]}</p>
-                    <p className='bg-blue-200 text-xl p-2 border-2 rounded-lg hover:bg-green-500 border-green-300'>{options[3]}</p>
+                    <p onClick={() => ToastAnswerbtn(options[0])} className='mb-2 mb-2 bg-blue-200 text-xl p-2 border-2 rounded-lg hover:bg-green-500 border-green-300'>{options[0]}</p>
+                    <p onClick={() => ToastAnswerbtn(options[1])} className='mb-2 bg-blue-200 text-xl p-2 border-2 rounded-lg hover:bg-green-500 border-green-300'>{options[1]}</p>
+                    <p onClick={() => ToastAnswerbtn(options[2])} className='mb-2 bg-blue-200 text-xl p-2 border-2 rounded-lg hover:bg-green-500 border-green-300'>{options[2]}</p>
+                    <p onClick={() => ToastAnswerbtn(options[3])} className='mb-2 bg-blue-200 text-xl p-2 border-2 rounded-lg hover:bg-green-500 border-green-300'>{options[3]}</p>
                 </div>
             </div>
+
+            <ToastContainer />
         </div>
+
     );
 };
 
