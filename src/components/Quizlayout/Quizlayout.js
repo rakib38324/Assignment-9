3bfcc78cf1602d39be78b6d2743040ecc6ca2034
rakib38324 <rakib3832@gmail.com>
2022-10-11 20:35:24 +0600

    Fixed the question text

--- src/components/Quizlayout/Quizlayout.js
+++ src/components/Quizlayout/Quizlayout.js
@@ -9,6 +9,9 @@ const Quizlayout = ({ topic }) => {
     const { question, options, correctAnswer
     } = topic;
 
+    const replace = question.replace("<p>", "");
+    const realquestion = replace.replace("</p>", "");
+
     const ToastAnswer = () => {
         toast(`${correctAnswer
             }`, {
@@ -37,7 +40,7 @@ const Quizlayout = ({ topic }) => {
             <div className='m-5 mt-20 sm:mt-10 sm:m-auto p-5 border-2 bg-blue-300 border-sky-600 rounded-xl sm:w-1/2 shadow-lg shadow-cyan-500/50'>
 
                 <div className='grid grid-cols-12 gap-1 col-end-1'>
-                    <p className='text-3xl p-4 col-span-11'>{question}</p>
+                    <p className='text-3xl p-4 col-span-11 font-bold'>{realquestion}</p>
                     <FontAwesomeIcon onClick={ToastAnswer} className="pl-4" icon={faEye}></FontAwesomeIcon>
                 </div>
                 <div className=' sm:grid grid-cols-2 gap-8 content-center '>
@@ -45,6 +48,8 @@ const Quizlayout = ({ topic }) => {
                     <p onClick={() => ToastAnswerbtn(options[1])} className='mb-2 bg-blue-200 text-xl p-2 border-2 rounded-lg hover:bg-green-500 border-green-300'>{options[1]}</p>
                     <p onClick={() => ToastAnswerbtn(options[2])} className='mb-2 bg-blue-200 text-xl p-2 border-2 rounded-lg hover:bg-green-500 border-green-300'>{options[2]}</p>
                     <p onClick={() => ToastAnswerbtn(options[3])} className='mb-2 bg-blue-200 text-xl p-2 border-2 rounded-lg hover:bg-green-500 border-green-300'>{options[3]}</p>
+                    
+                    
                 </div>
             </div>
 
